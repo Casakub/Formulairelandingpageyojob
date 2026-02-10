3941e57626b8db81a7d89face93008ad74f558e8
root <root@uxomnia.cloud>
2026-02-10 19:04:30 +0000

    Restore app files from guard branch

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { initAnalytics } from "./lib/analytics";
-import "./styles/globals.css";
+import "./index.css";
 
 initAnalytics();
 
 createRoot(document.getElementById("root")!).render(<App />);
+  
